2e50a6bebed75906090e6c9ff1061ffd1fb76cc9
iuemon83 <83.program+github@gmail.com>
2022-05-25 01:30:26 +0900

    v20220525.1

--- Cauldron.Client.Unity/Assets/Scripts/Config.cs
+++ Cauldron.Client.Unity/Assets/Scripts/Config.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 class Config
 {
-    public static readonly string Version = "20220521.1";
+    public static readonly string Version = "20220525.1";
 
     /// <summary>
     /// カードの画像を格納するディレクトリのパス
