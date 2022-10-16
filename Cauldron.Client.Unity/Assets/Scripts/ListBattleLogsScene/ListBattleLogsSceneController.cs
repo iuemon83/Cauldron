d748763a696b6481a04e5c583ace6c880c2d34b8
iuemon83 <83.program+github@gmail.com>
2022-10-16 18:02:49 +0900

    update

--- Cauldron.Client.Unity/Assets/Scripts/ListBattleLogsScene/ListBattleLogsSceneController.cs
+++ Cauldron.Client.Unity/Assets/Scripts/ListBattleLogsScene/ListBattleLogsSceneController.cs
@@ -5,7 +5,6 @@ using Cysharp.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.UI;
 
